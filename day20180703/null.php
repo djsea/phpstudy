98bb41f2caf43c60dd1ae68aa79b6b720eb59195
djsea <djsea@139.com>
2018-07-05 00:26:37 +0800

    05

---
+++ day20180703/null.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dujun
+ * Date: 2018/7/5
+ * Time: 上午12:04
+ */
+
+//$x='Hello world!';
+//$x=null;
+//var_dump($x);
+
+//$b = 3.1;
+//$c = true;
+//var_dump($b, $c);
+
+$a = array(1, 2, array("a", "b", "c"));
+var_dump($a);
